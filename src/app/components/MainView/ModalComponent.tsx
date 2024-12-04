17a2d1aafeeb71bce68eeecc862bb4e271a03cd1
jombi <jombi@duck.com>
2024-12-04 11:53:22 +0900

    feat :: separate files, fix major bugs

---
+++ src/app/components/MainView/ModalComponent.tsx
@@ -0,0 +1,198 @@
+import StepperComponent from "@/app/components/Stepper/StepperComponent";
+import Ratio from "@/app/components/Ratio";
+import QuestionnaireSelectButton from "@/app/components/Button/QuestionnaireSelectButton";
+import TitleDescriptionInput from "@/app/components/Input/TitleDescriptionInput";
+import Position from "@/app/components/Position";
+import {Circle} from "rc-progress";
+import useRatioDirectionStore from "@/app/store/useRatioDirectionStore";
+import useStepStore from "@/app/store/useStepStore";
+import {useCallback, useEffect, useState} from "react";
+import Cookies from "js-cookie";
+import axios from "axios";
+import {useSearchParams} from "next/navigation";
+import {io, Socket} from "socket.io-client";
+
+export default function ModalComponent() {
+    const paletteAxios = axios.create({
+        baseURL: process.env.NEXT_PUBLIC_API_URL,
+        validateStatus: () => true,
+    });
+
+    const searchParams = useSearchParams();
+    const roomId = searchParams.get('room');
+    const [qna, setQna] = useState<any[]>();
+    const { ratio, direction, setRatio } = useRatioDirectionStore();
+    const { step, setStep } = useStepStore();
+    const [Value, setValue] = useState<string>('');
+    const [generating, setGenerating] = useState<boolean>(false);
+    const [position, setPosition] = useState<number>(-1);
+    const [max, setMax] = useState<number>(-1);
+    const [genStep, setGenStep] = useState<number>(-1);
+    const connectWebSocket = useCallback(() => {
+        const socket: Socket = io(`localhost:3336/conversion`, {
+            transports: ['websocket'],
+        });
+
+        socket.on('connect', () => {
+            console.log('Socket connected');
+            socket.emit('wstoio', {
+                roomId: roomId,
+                token: Cookies.get('access_token')
+            });
+        });
+
+        socket.on('message', (message: any) => {
+            if (message.type === 'NEW_CHAT') {
+                setMessageList((prevState) => [...prevState, message]);
+            } else if (message.type === 'IMAGE_PROGRESS') {
+                setGenStep(message.value);
+                setMax(message.max);
+            } else if (message.type === 'GENERATE_STATUS') {
+                setPosition(message.position);
+                setGenerating(message.generating);
+            }
+        });
+
+        return () => {
+            socket.disconnect();
+        };
+    }, [roomId]);
+    
+    useEffect(() => {
+        const connect = connectWebSocket();
+        return () => {
+            connect();
+        };
+    }, []);
+
+    const getQna = async (id: string) => {
+        try {
+            await paletteAxios.get(`${process.env.NEXT_PUBLIC_API_URL}/room/${id}/qna`, {
+                headers: {
+                    'x-auth-token': Cookies.get('access_token')
+                }
+            }).then((res) => {
+                if (res.status === 200) {
+                    const qnas = res.data;
+                    setQna(qnas.data);
+                    setStep(qnas.data.filter((q: any) => q.answer !== null).length);
+                    setRatio(qnas.data.find((it: any) => it.promptName === 'aspect_ratio').answer?.choiceId ?? 'DISPLAY');
+                }
+            })
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    function mapToPercentage(value: number): number {
+        value = Math.max(0, value);
+
+        return value / max * 100; // bruh why this is complex
+    }
+    const regenImage = async () => {
+        try {
+            await paletteAxios.post(`${process.env.NEXT_PUBLIC_API_URL}/room/${roomId}/regen`, {}, {
+                headers: { 'x-auth-token': Cookies.get('access_token') }
+            })
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    
+    const getChat = async (roomId: string) => {
+        try {
+            const res = await paletteAxios.get(`${process.env.NEXT_PUBLIC_API_URL}/chat/${roomId}`, {
+                headers: { 'x-auth-token': Cookies.get('access_token') }
+            })
+            
+            const dat = [...res.data.data];
+            dat.reverse();
+            setMessageList(dat);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    
+    useEffect(() => {
+        if (roomId) {
+            getQna(roomId);
+            getChat(roomId);
+        }
+    }, [roomId])
+    
+    const [messageList, setMessageList] = useState<any[]>([]);
+    const messageListProgress = messageList.filter((message) => message.value !== undefined);
+    console.log(messageListProgress);
+    
+    return roomId ? (
+        <div className={`palette-mainBox`}>
+            <div style={{position: 'absolute', width: '100%', height: '100%'}}>
+                <StepperComponent/>
+            </div>
+            {
+                step < 2 ? (
+                    <div style={{position: 'relative'}}>
+                        <Ratio ratio={ratio} direction={direction}/>
+                        <QuestionnaireSelectButton step={step}/>
+                    </div>
+                ) : null
+            }
+            {
+                step === 2 ? (
+                    <TitleDescriptionInput
+                        type={'Description'}
+                        content={messageList[5]?.message || ''}
+                        value={Value}
+                        setValue={setValue}
+                    />
+                ) : null
+            }
+            {
+                step === 3 ? (
+                    <TitleDescriptionInput
+                        type={'Title'}
+                        content={messageList[7]?.message || ''}
+                        value={Value}
+                        setValue={setValue}
+                    />
+                ) : null
+            }
+            {
+                step === 4 ? (
+                    <Position content={messageList[6]?.message || '포스터 제목의 위치는 어디에 두시겠습니까?'}/>
+                ) : null
+            }
+            {
+                step === 5 ? (
+                    <div className={`complete`}>
+                        {
+                            generating ? (
+                                <div className="circle-progress-wrapper">
+                                    <Circle
+                                        percent={mapToPercentage(genStep)}
+                                        strokeWidth={5}
+                                        strokeColor="#3E4AED"
+                                        trailColor={'#D3D3D3'}
+                                        className="progress"
+                                    />
+                                    <div className="progress-text">
+                                        {`이미지 제작 중...`}
+                                        {position > 0 ? ` (현재 대기열의 ${position}번째 순서입니다.)` : ''}
+                                    </div>
+                                </div>
+                            ) : messageList.length > 0 && messageList[messageList.length - 1].resource === 'IMAGE' ? (
+                                <div className={`finalBox`}>
+                                    <img src={messageList[messageList.length - 1].message} alt={'image'}/>
+                                    <div className={`regenBtn`} onClick={regenImage}>이미지 재생성</div>
+                                </div>
+                            ) : null
+                        }
+                    </div>
+                ) : null
+            }
+        </div>
+    ) : (
+        <div>
+            왼쪽 창에서 제작할 방을 선택 해 주세요.
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,48 +6,26 @@ import {IoIosSearch} from "react-icons/io";
 import {HiMiniInbox} from "react-icons/hi2";
 import {FaPlus} from "react-icons/fa";
 import {MdMoreHoriz} from "react-icons/md";
-import {useCallback, useEffect, useState} from "react";
+import React, {Suspense, useEffect, useState} from "react";
 import axios from "axios";
 import Cookies from "js-cookie";
 import useUserStore from "@/app/store/useUserStore";
 import useRoomStore from "@/app/store/useRoomStore";
-import StepperComponent from "@/app/components/Stepper/StepperComponent";
-import Ratio, {RatioType} from "@/app/components/Ratio";
-import QuestionnaireSelectButton from "@/app/components/Button/QuestionnaireSelectButton";
-import useRatioDirectionStore from "@/app/store/useRatioDirectionStore";
-import useStepStore from "@/app/store/useStepStore";
-import {useSearchParams} from "next/navigation";
-import {io, Socket} from "socket.io-client";
-import TitleDescriptionInput from "@/app/components/Input/TitleDescriptionInput";
-import Position from "@/app/components/Position";
-import {Circle} from "rc-progress";
-import Image from "next/image";
-
-interface MessageItem {
-  regenScope: boolean;
-  message: string;
-  resource: any;
-  type: string;
-  value: number;
-  max: number;
-  position: number;
-}
+import ModalComponent from "@/app/components/MainView/ModalComponent";
 
 export default function Home() {
+  const paletteAxios = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    validateStatus: () => true,
+  });
+  
   const { user, setUser } = useUserStore();
   const { roomList, setRoomList } = useRoomStore();
-  const { ratio, direction } = useRatioDirectionStore();
-  const { step, setStep } = useStepStore();
-  const [qna, setQna] = useState<any[]>();
-  const [messageList, setMessageList] = useState<MessageItem[]>([]);
-  const [Value, setValue] = useState<string>('');
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-  const searchParams = useSearchParams();
-  const roomId = searchParams.get('room');
 
   const getUserData = async () => {
     try {
-      await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/info/me`, {
+      await paletteAxios.get(`${process.env.NEXT_PUBLIC_API_URL}/info/me`, {
         headers: { 'x-auth-token': Cookies.get('access_token') }
       })
           .then((res) => {
@@ -62,7 +40,7 @@ export default function Home() {
 
   const getRoomLists = async () => {
     try {
-      await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/room/list`, {
+      await paletteAxios.get(`${process.env.NEXT_PUBLIC_API_URL}/room/list`, {
         headers: {
           'x-auth-token': Cookies.get('access_token')
         }
@@ -82,56 +60,9 @@ export default function Home() {
     }
   };
 
-  const connectWebSocket = useCallback(() => {
-    const socket: Socket = io(`localhost:3336/conversion`, {
-      transports: ['websocket'],
-    });
-
-    socket.on('connect', () => {
-      console.log('Socket connected');
-      socket.emit('wstoio', {
-        roomId: roomId,
-        token: Cookies.get('access_token')
-      });
-    });
-
-    socket.on('message', (message: MessageItem) => {
-      setMessageList((prevState) => [...prevState, {
-        message: message.message,
-        resource: message.resource,
-        type: message.type,
-        position: message.position,
-        regenScope: message.regenScope,
-        value: message.value,
-        max: message.max,
-      }])
-    });
-
-    return () => {
-      socket.disconnect();
-    };
-  }, [roomId]);
-
-  const getQna = async (id: string) => {
-    try {
-      await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/room/${id}/qna`, {
-        headers: {
-          'x-auth-token': Cookies.get('access_token')
-        }
-      }).then((res) => {
-        if (res.status === 200) {
-          setQna(res.data.data);
-          setStep(res.data.data.filter((q: any) => q.answer !== null).length);
-        }
-      })
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
   const addRoom = async () => {
     try {
-      await axios.post(`${(process.env.NEXT_PUBLIC_API_URL)}/room`, {}, {
+      await paletteAxios.post(`${(process.env.NEXT_PUBLIC_API_URL)}/room`, {}, {
         headers: {
           'x-auth-token': Cookies.get('access_token')
         }
@@ -146,44 +77,14 @@ export default function Home() {
     }
   }
 
-  function mapToPercentage(value: number): number {
-    const minValue = 1;
-    const maxValue = 6;
-    const targetMin = 0;
-    const targetMax = 100;
-
-    if (value < minValue || value > maxValue) {
-      throw new Error(`Value must be between ${minValue} and ${maxValue}.`);
-    }
-
-    return ((value - minValue) / (maxValue - minValue)) * (targetMax - targetMin) + targetMin;
-  }
-
-  const regenImage = async () => {
-    try {
-      await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/room/${roomId}/regen`, {}, {
-        headers: { 'x-auth-token': Cookies.get('access_token') }
-      })
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  useEffect(() => {
-    if (roomId) {
-      getQna(roomId);
-    }
-  }, [roomId])
-
   useEffect(() => {
     const token = Cookies.get('access_token');
     setIsLoggedIn(!!token);
     getUserData();
     getRoomLists();
-    const socketCleanup = connectWebSocket();
     const refreshTokenIntervalId = setInterval(async () => {
       try {
-        await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/auth/session`, {
+        await paletteAxios.get(`${process.env.NEXT_PUBLIC_API_URL}/auth/session`, {
           headers: {
             'x-auth-token': Cookies.get('access_token'),
           },
@@ -196,12 +97,9 @@ export default function Home() {
     }, 180000);
 
     return () => {
-      socketCleanup;
       clearInterval(refreshTokenIntervalId);
     }
   }, []);
-  const messageListProgress = messageList.filter((message) => message.value !== undefined);
-  console.log(messageListProgress[messageListProgress.length - 1]);
   return (
       <main className={`palette-container`}>
         <div className={`palette-panel`}>
@@ -255,74 +153,9 @@ export default function Home() {
             <h1>로그인을 하여 Pallete가 제공하는 홍보물을 만들어 보세요.</h1>
             <div className={`signBtn`}>Login to Palette</div>
           </div>
-          <div className={`palette-mainBox`}>
-            <div style={{position: 'absolute', width: '100%', height: '100%'}}>
-              <StepperComponent />
-            </div>
-            {
-              step < 2 ? (
-                  <div style={{position: 'relative'}}>
-                    <Ratio ratio={ratio} direction={direction}/>
-                    <QuestionnaireSelectButton step={step}/>
-                  </div>
-              ) : null
-            }
-            {
-              step === 2 ? (
-                  <TitleDescriptionInput
-                      type={'Description'}
-                      content={messageList[5]?.message || ''}
-                      value={Value}
-                      setValue={setValue}
-                  />
-              ) : null
-            }
-            {
-              step === 3 ? (
-                  <TitleDescriptionInput
-                      type={'Title'}
-                      content={messageList[7]?.message || ''}
-                      value={Value}
-                      setValue={setValue}
-                  />
-              ) : null
-            }
-            {
-              step === 4 ? (
-                  <Position content={messageList[6]?.message || '포스터 제목의 위치는 어디에 두시겠습니까?'}/>
-              ) : null
-            }
-            {
-              step === 5 ? (
-                  <div className={`complete`}>
-                    {
-                      messageListProgress.length > 0 && messageListProgress[messageListProgress.length - 1].value !== 6 ? (
-                          <div className="circle-progress-wrapper">
-                            <Circle
-                                percent={mapToPercentage(Number(messageListProgress.length > 0 ? messageListProgress[messageListProgress.length - 1].value : 1))}
-                                strokeWidth={5}
-                                strokeColor="#3E4AED"
-                                trailColor={'#D3D3D3'}
-                                className="progress"
-                            />
-                            <div className="progress-text">
-                              {`이미지 제작 중...`}
-                            </div>
-                          </div>
-                      ) : null
-                    }
-                    {
-                      messageListProgress[messageListProgress.length - 1]?.value === 6 && messageList[messageList.length-1].resource === 'IMAGE' ? (
-                          <div className={`finalBox`}>
-                            <img src={messageList[messageList.length - 1].message} alt={'image'}/>
-                            <div className={`regenBtn`} onClick={regenImage}>이미지 재생성</div>
-                          </div>
-                      ) : null
-                    }
-                  </div>
-              ) : null
-            }
-          </div>
+          <Suspense>
+            <ModalComponent />
+          </Suspense>
         </div>
       </main>
   );
